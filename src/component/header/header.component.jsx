dbe78224c14858951296d25548335f87666ae271
augiemillares <augie@acadaca.com>
2020-03-28 18:29:40 -0400

    cart dropdown

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -5,34 +5,41 @@ import {connect} from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
 
 import {ReactComponent as Logo} from '../../asset/logo.svg';
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => {
+const Header = ({currentUser, cartHidden}) => {
     return(
         <div className='header'>
             <Link className='logo-container' to='/'>
                 <Logo className='logo'/>
             </Link>
-            <div className='option-container'>
-                <div className='options'>
-                    <Link className='option' to='/shop'>SHOP</Link>
-                    <Link className='option' to='/shop'>CONTACT</Link>
-                    {
-                        currentUser ?
-                            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                            :
-                            <Link className='option' to='/signin'>SIGN IN</Link>
-                    }
-                </div>
+            <div className='options'>
+                <Link className='option' to='/shop'>SHOP</Link>
+                <Link className='option' to='/shop'>CONTACT</Link>
+                {
+                    currentUser ?
+                        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                        :
+                        <Link className='option' to='/signin'>SIGN IN</Link>
+                }
+                <CartIcon/>
             </div>
+            {
+                cartHidden ? null : <CartDropdown/>
+            }
+
         </div>
     );
 }
 
-const mapStateToProp = state => (
+const mapStateToProp = ({user:{currentUser}, cart:{hidden}}) => (
         {
-            currentUser : state.user.currentUser
+            currentUser,
+            cartHidden: hidden
         }
     );
 
